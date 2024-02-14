12e441124e96009a534a7a1be4ecf20cfc4565b1
Hemant Bhargav <hsharma9667@gmail.com>
2024-02-14 22:28:19 +0530

    add map and contact section

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import Nav from "./components/navbar/Nav"
 import Heropage from "./components/heropage/Heropage"
 import Team from "./components/ourteam/Team"
 import Customer_review from "./components/whyus/Customer_review"
+import Map_contact from "./components/map & contact/Map_contact"
 export default function App(){
   return(
     <>
     <Nav />
     <Heropage />
     <Customer_review />
+    <Map_contact />
     <Team />
     </>
   )
