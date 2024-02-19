9271f025c2ba4541bfae5bad5c053743f0311123
hemant-7802 <hsharma9667@gmail.com>
2024-02-19 13:19:40 +0530

    updates in components

--- src/components/map & contact/Map_contact.jsx
+++ src/components/map & contact/Map_contact.jsx
@@ -9,6 +9,14 @@ import {Button} from "@nextui-org/react";
 import { FaRegUser } from "react-icons/fa";
 import { FaPhoneAlt } from "react-icons/fa";
 
+// const inputdetails = [
+//   {
+//     type : 'text',
+//     label : 'Name',
+//     placeholder : 'Enter your name',
+//     startContent : ''
+//   },
+// ]
 
 function Map_contact() {
 
@@ -26,11 +34,11 @@ function Map_contact() {
 
   return (
     <div id='contactus' className='flex flex-wrap justify-evenly align-middle iPadPro12.9:flex-nowrap mb-6 gap-8'>
-      <div className='flex flex-wrap flex-col gap-7 p-4'>
-        <h1 className='text-center text-4xl font-medium iPhone4and4S:text-2xl iPhone6and6Sand7and8:text-3xl'>Connecting Through Coordinates</h1>
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3570.869123423727!2d77.0234318755361!3d26.492158478037002!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x397187cdd77c430d%3A0x8c19e77710276b81!2sKedar%20Medical%20Store!5e0!3m2!1sen!2sin!4v1707912206057!5m2!1sen!2sin" width="550" height="450" style={{border:0}} loading="lazy" className='iPhone4and4S:w-[300px] iPhone4and4S:h-[350px] iPhone6and6Sand7and8:w-[350px] iPhone6+and7+and8+:w-[390px]'></iframe>
+      <div className='flex flex-wrap flex-col gap-8 p-4'>
+        <h1 className='maptitle text-center text-4xl font-medium iPhone4and4S:text-2xl iPhone6and6Sand7and8:text-3xl'>Connecting Through Coordinates</h1>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3570.8454596064116!2d77.01149457546765!3d26.492920276899905!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x397186033b68597d%3A0x9dea9d9a65719a1a!2sK-Tech%20Education(karauli%20technical%20education%20%26%20computer%20hub!5e0!3m2!1sen!2sin!4v1708328824422!5m2!1sen!2sin" width="700" height="500" style={{border:0}} loading="lazy" className='iPhone4and4S:w-[300px] iPhone4and4S:h-[350px] iPhone6and6Sand7and8:w-[350px] iPhone6+and7+and8+:w-[390px]'></iframe>
       </div>
-      <div className="flex flex-col gap-6 h-auto min-w-[300px]">
+      <div className="flex flex-col gap-8 h-auto w-[450px] min-w-[300px] p-4">
         <h1 className='capitalize text-center text-4xl font-semibold mb-4 text-purple-600'>contact us</h1>
         <Input
         value={namevalue}
@@ -62,7 +70,7 @@ function Map_contact() {
         value={phonevalue}
         type='text' 
         label='Mobile No.' 
-        placeholder='Enter your Phone Number' 
+        placeholder='Enter your Mobile Number' 
         labelPlacement='outside'
         isClearable
         onValueChange={setphonevalue}
