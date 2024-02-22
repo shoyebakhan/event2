5ea6552ee89f14727a2c867f3de02802ad2b6c73
shiva8769 <varunupadyay7@gmail.com>
2024-02-22 12:34:09 +0530

    dark theme enabled

--- src/_Details.jsx
+++ src/_Details.jsx
@@ -357,6 +357,16 @@ export const reviews = {
       avatarSrc:
         "https://images.unsplash.com/photo-1502685104226-ee32379fefbe?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=256&h=256&q=80",
     },
+    {
+      id: 4,
+      rating: 5,
+      content: `
+          <p>This is the bag of my dreams. I took it on my last vacation and was able to fit an absurd amount of snacks for the many long and hungry flights.</p>
+        `,
+      author: "Mohit Saraswat",
+      avatarSrc:
+        "https://images.unsplash.com/photo-1502685104226-ee32379fefbe?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=256&h=256&q=80",
+    },
     // More reviews...
   ],
 };
